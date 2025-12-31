2060c1b184f92d8141981179b33b203844ae20bb
Your Name <your_email@example.com>
2025-12-31 23:28:54 +0100

    debug(api): include roles in getagents error response

--- app/Http/Controllers/ApiUsersController.php
+++ app/Http/Controllers/ApiUsersController.php
@@ -46,7 +46,7 @@ class ApiUsersController extends Controller
             if (\Auth::user()->type == 'Agents' || \Auth::user()->type == 'Agent' || \Auth::user()->hasRole('Agent')) {
                 return DB::table('users')->select('id', 'created_by', 'created_by_user', 'email', 'name', 'phone', 'type', 'avatar', 'gender')->orderBy('name', 'ASC')->where('id', \Auth::user()->id)->get();
             } else {
-                return 'error: ' . \Auth::user()->type;
+                return 'error: Type=[' . \Auth::user()->type . '] Roles=' . json_encode(\Auth::user()->getRoleNames());
             }
         }
     }
