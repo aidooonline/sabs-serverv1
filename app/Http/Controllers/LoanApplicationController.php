12f92cc4d03ae65a53719669bb0235062a561e32
Your Name <your_email@example.com>
2026-04-20 09:29:49 +0100

    Fix: Added missing DB facade import causing 500 error in Loan History

--- app/Http/Controllers/LoanApplicationController.php
+++ app/Http/Controllers/LoanApplicationController.php
@@ -7,6 +7,7 @@ use App\LoanApplication;
 use App\LoanProduct;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class LoanApplicationController extends Controller
