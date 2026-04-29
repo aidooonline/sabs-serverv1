84e28ee4f001721dacd76b1dc0bac84b644ffd85
Your Name <your_email@example.com>
2026-04-29 07:31:03 +0100

    Revert PHP 8.3 patches for PHP 7.4 environment

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,10 +14,6 @@ class Kernel extends HttpKernel
     public function bootstrap()
     {
         parent::bootstrap();
-
-        if (PHP_VERSION_ID >= 80100) {
-            error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
-        }
     }
 
     /**
